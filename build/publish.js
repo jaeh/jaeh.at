be059af1a9fca6c47f1c146430000eaf9d8fd5c8
Jascha Ehrenreich <jascha@jaeh.at>
2017-01-03 22:41:08 +0100

    update git command to force push

--- build/publish.js
+++ build/publish.js
@@ -8,13 +8,14 @@ const log = require('./log')
 const origin = conf.GIT_ORIGIN
 const branch = conf.GIT_BRANCH
 
-const cmd = `git subtree push --prefix public ${ origin } ${ branch }`
+// const cmd = `git subtree push --rejoin --squash --prefix public ${ origin } ${ branch }`
+const cmd = `git push ${ origin } \`git subtree split --prefix public ${ branch }\`:${ branch } --force`
 
 
 log('executing:', cmd)
 exec(cmd, (err, res) => {
   if (err) {
-    logError(err)
+    log.error(err)
     return
   }
 
