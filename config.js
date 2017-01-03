c0203341eb0e77ab04d1207f5cd157319740f327
Jascha Ehrenreich <jascha@jaeh.at>
2017-01-03 21:49:15 +0100

    set GIT_BRANCH to master for this project

--- config.js
+++ config.js
@@ -2,7 +2,10 @@ const path = require('path')
 
 const { TRANSPILE_HTML, TRANSPILE_CSS } = require('./build/transpilers')
 
+const GIT_BRANCH = 'master'
+
 module.exports = {
   TRANSPILE_HTML,
   TRANSPILE_CSS,
+  GIT_BRANCH,
 }
