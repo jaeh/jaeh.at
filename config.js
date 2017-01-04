5b5da9c337bbd8c36fc152da739b3ac8dfc08dc9
Jascha Ehrenreich <jascha@jaeh.at>
2017-01-04 01:11:08 +0100

    commit to gh-pages

--- config.js
+++ config.js
@@ -2,10 +2,7 @@ const path = require('path')
 
 const { TRANSPILE_HTML, TRANSPILE_CSS } = require('./build/transpilers')
 
-const GIT_BRANCH = 'master'
-
 module.exports = {
   TRANSPILE_HTML,
   TRANSPILE_CSS,
-  GIT_BRANCH,
 }
