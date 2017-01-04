28d1f1e6ce0a422d118e04da299b943a86b374c6
Jascha Ehrenreich <jascha@jaeh.at>
2017-01-04 01:57:47 +0100

    use config vars in html files

--- config.js
+++ config.js
@@ -2,7 +2,18 @@ const path = require('path')
 
 const { TRANSPILE_HTML, TRANSPILE_CSS } = require('./build/transpilers')
 
+
+
 module.exports = {
   TRANSPILE_HTML,
   TRANSPILE_CSS,
+  FIRST_NAME: 'jascha',
+  LAST_NAME: 'ehrenreich',
+  JOB_DESCRIPTION: 'programmer & streetartist',
+  DESCRIPTION: [
+    'i juggle with code since fifteen,',
+    'with cones, balls and rings since twentyfive,',
+    'and life juggles with me since around thirtyfive years.',
+    'i enjoy all of it.',
+  ],
 }
