ff02afa9eb2d31109e8b999cc8cff87715508bb8
Jascha Ehrenreich <jascha@jaeh.at>
2017-01-15 18:13:42 +0100

    add browserify transpiler

--- config.js
+++ config.js
@@ -6,6 +6,7 @@ module.exports = {
   TRANSPILERS: {
     HTML: transpilers.PUG,
     CSS: transpilers.STYLUS,
+    JS: transpilers.BROWSERIFY,
   },
   FIRST_NAME: 'jascha',
   LAST_NAME: 'ehrenreich',
