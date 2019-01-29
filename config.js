ab0e4e9b980926d9abfac90211f6f0c38aec2539
Jascha <jascha@jaeh.at>
2019-01-29 03:14:18 +0100

    update themes

--- config.js
+++ config.js
@@ -1,3 +1,15 @@
 module.exports = {
   ROOT: 'src',
+  THEME: 'jaeh',
+  THEME_VARS: {
+    black: '#010101',
+    white: '#FEFEFE',
+    textColor: '#DEDEDE',
+    backgroundColor: '#010101',
+    linkColor: '#F57C00',
+    linkColorLight: '#FB8C00',
+    linkColorDark: '#EF6C00',
+    borderColor: '#F57C00',
+    maxWidth: '1200px',
+  },
 }
---
+++ src/assets/themes/jaeh/index.js
@@ -0,0 +1,158 @@
+const theme = vars => ({
+  '::selection': {
+    backgroundColor: vars.textColor,
+    color: vars.backgroundColor,
+    textShadow: 'none',
+  },
+  body: {
+    backgroundColor: vars.backgroundColor,
+    border: 0,
+    color: vars.textColor,
+    fontFamily: 'Ubuntu, "Segoe UI", roboto, "Lucida Grande", "Helvetica Neue", Helvetica, Arial, sans-serif',
+    fontSize: '18px',
+    fontWeight: '400',
+    height: '100%',
+    lineHeight: '2em',
+    margin: 0,
+    overflowY: 'scroll',
+    padding: 0,
+    position: 'absolute',
+    verticalAlign: 'baseline',
+    width: '100%',
+  },
+
+  'h2, h3': {
+    margin: '2em 0 0',
+  },
+
+  '.communications': {
+    'h2, h3': {
+      margin: 0,
+    },
+  },
+
+  h2: {
+    fontSize: '1.3em',
+  },
+  h3: {
+    fontSize: '1.1em',
+  },
+  a: {
+    color: vars.linkColorDark,
+    textDecoration: 'none',
+    transition: 'color 500ms, box-shadow 500ms',
+    boxShadow: `inset 0 -1px 0 0 ${vars.backgroundColor}, inset 0 -2px 0 0 ${vars.linkColorLight}`,
+    textShadow: `1px 1px 0 ${vars.backgroundColor}, -1px 1px 0 ${vars.backgroundColor}, 2px 0 0 ${vars.backgroundColor}, -2px 0 0 ${vars.backgroundColor}`,
+
+    '&:hover': {
+      color: vars.linkColorLight,
+      boxShadow: `inset 0 -1px 0 0 ${vars.backgroundColor}, inset 0 -2px 0 0 ${vars.linkColorDark}`,
+      textShadow: `1px 1px 0 ${vars.backgroundColor}, -1px 1px 0 ${vars.backgroundColor}, 2px 0 0 ${vars.backgroundColor}, -2px 0 0 ${vars.backgroundColor}`,
+    },
+  },
+
+  ul: {
+    listStyle: 'none',
+  },
+
+  '#main': {
+    margin: '8vw auto 0',
+    maxWidth: vars.maxWidth,
+    minHeight: '95vh',
+    width: '90%',
+  },
+
+  '.article': {
+    clear: 'both',
+    display: 'inline-block',
+    minHeight: '40vh',
+  },
+
+  '.section': {
+    clear: 'both',
+    margin: '2em 0 1em',
+
+    h3: {
+      margin: '1em 0 .2em',
+    },
+
+    '&.unlicense': {
+      a: {
+        display: 'inline-block',
+        lineHeight: '1.1',
+      },
+    },
+    '&.page': {
+      maxWidth: '100%',
+    },
+  },
+
+  '.footer': {
+    clear: 'both',
+    fontSize: '.7em',
+    margin: '1em 0 0',
+    textAlign: 'center',
+    width: '100%',
+
+    '.anarchy': {
+      boxShadow: 'none',
+      color: vars.textColor,
+      fontSize: '1.1em',
+      fontWeight: 800,
+      textDecoration: 'none',
+      textShadow: 'none',
+    },
+  },
+
+  '@media screen and (min-width: 800px': {
+    body: {
+      textAlign: 'left',
+
+      '.header__image': {
+        float: 'left',
+        height: '29vw',
+        margin: '0 2em 3em 0',
+        width: '29vw',
+      },
+
+      '.section': {
+        clear: 'none',
+        display: 'inline-block',
+        float: 'left',
+        width: '50%',
+
+        '&.projects, &.congregations': {
+          clear: 'both',
+          width: '100%',
+        },
+
+        '&.unlicense': {
+          maxWidth: '100%',
+        },
+
+        '.footer': {
+          margin: '5em 0',
+        },
+      },
+    },
+  },
+
+  '@media screen and (min-width: 1100px)': {
+    body: {
+      '.section': {
+        '&.communications, &.congregations, &.projects, &.unlicense': {
+          clear: 'none',
+          margin: '0 2em 0 0',
+          maxWidth: '25%',
+          width: 'auto',
+        },
+        '&.projects, &.congregations': {
+          maxWidth: 'inherit',
+          width: '100%',
+        },
+      },
+    },
+  },
+})
+
+module.exports = theme
