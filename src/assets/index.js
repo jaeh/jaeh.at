e970f72803c196c52a9ebe233e45956edf452db7
Jascha <jascha@jaeh.at>
2019-04-21 07:33:55 +0200

    remove unused dep

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,6 +1,5 @@
 module.exports = {
   Header: require('./Header'),
   Footer: require('./Footer'),
-  // Contact: require('@magic-modules/contact'),
   LanguageSwitch: require('./LanguageSwitch'),
 }
