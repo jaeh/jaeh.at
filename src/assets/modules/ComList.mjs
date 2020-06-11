e728bd13904d75a695b3a50dc22c66c3d0aac53b
jascha <jascha@jaeh.at>
2020-06-11 20:28:14 +0200

    add modules/ComList

---
+++ src/assets/modules/ComList.mjs
@@ -0,0 +1,15 @@
+export const View = (title = 'communications') =>
+  div({ class: 'section communications' }, [
+    h2(title),
+    ul([
+      li(['email: ', Link({ to: 'mailto:jascha@jaeh.at', text: 'jascha@jaeh.at' })]),
+      li(['github: ', Link({ to: 'https://github.com/jaeh', text: '@jaeh' })]),
+      li(['gitlab: ', Link({ to: 'https://gitlab.com/jaeh', text: '@jaeh' })]),
+      li(['keybase: ', Link({ to: 'https://keybase.io/jascha', text: 'jascha' })]),
+      li(['twitter: ', Link({ to: 'https://twitter.com/weirdpress', text: '@weirdpress' })]),
+      li([
+        'facebook: ',
+        Link({ to: 'https://facebook.com/jascha.ehrenreich', text: '/jaschaehrenreich' }),
+      ]),
+    ]),
+  ])
