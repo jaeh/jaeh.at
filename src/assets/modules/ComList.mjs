f7e6e6f35e1b26fe12123d2f7ff1c12db9b8962d
jaeh <jascha@jaeh.at>
2021-09-02 18:37:31 +0200

    assets/modules/ComList: add npm link, remove @ from names

--- src/assets/modules/ComList.mjs
+++ src/assets/modules/ComList.mjs
@@ -3,9 +3,10 @@ export const View = (title = 'communications') =>
     h2(title),
     ul([
       li(['email: ', Link({ to: 'mailto:jascha@jaeh.at', text: 'jascha@jaeh.at' })]),
-      li(['github: ', Link({ to: 'https://github.com/jaeh', text: '@jaeh' })]),
-      li(['gitlab: ', Link({ to: 'https://gitlab.com/jaeh', text: '@jaeh' })]),
+      li(['github: ', Link({ to: 'https://github.com/jaeh', text: 'jaeh' })]),
+      li(['gitlab: ', Link({ to: 'https://gitlab.com/jaeh', text: 'jaeh' })]),
+      li(['npm: ', Link({ to: 'https://www.npmjs.com/~jascha', text: 'jascha' })]),
       li(['keybase: ', Link({ to: 'https://keybase.io/jascha', text: 'jascha' })]),
-      li(['twitter: ', Link({ to: 'https://twitter.com/weirdpress', text: '@weirdpress' })]),
+      li(['twitter: ', Link({ to: 'https://twitter.com/weirdpress', text: 'weirdpress' })]),
     ]),
   ])
