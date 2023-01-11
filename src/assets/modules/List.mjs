aebfdefdf64a3104f270640fcc318bcdf0cfb1d3
jaeh <jascha@jaeh.at>
2023-01-11 22:32:36 +0100

    assets/modules/List: move styles into module, use Picture

--- src/assets/modules/List.mjs
+++ src/assets/modules/List.mjs
@@ -1,10 +1,10 @@
 export const View = (items = [], children = []) =>
   ul(
     { class: 'List' },
-    [...items, ...children].map(({ src, title, to, text }) => [
+    [...items, ...children].map(({ name, title, to, text }) => [
       li([
-        src && Img({ width: 60, height: 60, src }),
-        title && div([h3([Link({ to }, title)]), p(text)]),
+        name && div(Picture({ width: 60, height: 60, name, ext: 'png' })),
+        title && div([h3([Link({ to }, title)]), P(text)]),
       ]),
     ]),
   )
@@ -12,8 +12,21 @@ export const View = (items = [], children = []) =>
 export const style = {
   width: '100%',
 
+  li: {
+    margin: '0 0 2em',
+  },
+
   h3: {
     margin: 0,
     padding: 0,
+    lineHeight: 1,
+    margin: '0 0 0.5em',
+  },
+
+  img: {
+    clear: 'left',
+    float: 'left',
+    margin: '0 1em 0.2em 0',
+    height: 'auto',
   },
 }
