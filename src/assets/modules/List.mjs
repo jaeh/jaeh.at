b361a815a506da23743f92ab9a9b7807cbba0812
jaeh <jascha@jaeh.at>
2023-08-16 19:56:17 +0200

    assets/modules/List: add link in h3 only if needed, add text color style for h3 without link

--- src/assets/modules/List.mjs
+++ src/assets/modules/List.mjs
@@ -4,12 +4,12 @@ export const View = (items = [], children = []) =>
     [...items, ...children].map(({ name, width = 60, height = 60, title, to, text }) => [
       li([
         name && div(Picture({ width, height, name, ext: 'png' })),
-        title && div([h3([Link({ to }, title)]), P(text)]),
+        title && div([h3([to ? Link({ to }, title) : title]), P(text)]),
       ]),
     ]),
   )
 
-export const style = {
+export const style = vars => ({
   width: '100%',
 
   li: {
@@ -21,6 +21,11 @@ export const style = {
     padding: 0,
     lineHeight: 1,
     margin: '0 0 0.5em',
+    color: vars.link.dark,
+
+    '.light &&': {
+      color: vars.link.light,
+    },
   },
 
   img: {
@@ -29,4 +34,4 @@ export const style = {
     margin: '0 1em 0.2em 0',
     height: 'auto',
   },
-}
+})
